c58857bc1ceab7b2473de54616d9f8d5c43fa8e4
Maksim Dorogoi <monarch2013@mail.ru>
2024-11-01 20:26:47 +1100

    Container и вид header и footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,71 @@
+// src/app/components/Footer.tsx
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <footer>
-            Нижний колонтитул
-        </footer>
-    );
+  return (
+    <footer className="bg-[#3D4F64] text-white py-8">
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
+      {/* Первая колонка */}
+      <div className="flex flex-col items-start">
+        <a href="/" className="flex flex-col items-start">
+          <img src="/logo-footer.svg" alt="Логотип" className="mb-2" />
+          <p>© Technolight 2015-2021</p>
+        </a>
+      </div>
+
+        {/* Вторая колонка */}
+        <div className="flex flex-col items-start">
+          <h3 className="font-bold mb-2">Каталог товаров</h3>
+          <ul className="list-none space-y-1">
+            <li><a href="#" className="hover:underline">Трековые светильники</a></li>
+            <li><a href="#" className="hover:underline">Встраиваемые светильники</a></li>
+            <li><a href="#" className="hover:underline">Профильные светильники</a></li>
+            <li><a href="#" className="hover:underline">Фигурные светильники</a></li>
+            <li><a href="#" className="hover:underline">Светильники Армстронг</a></li>
+            <li><a href="#" className="hover:underline">Армстронг с равномерной засветкой</a></li>
+            <li><a href="#" className="hover:underline">Производители</a></li>
+          </ul>
+        </div>
+
+        {/* Третья колонка */}
+        <div className="flex flex-col items-start">
+          <h3 className="font-bold mb-2">Как заказать</h3>
+          <ul className="list-none space-y-1">
+            <li><a href="#" className="hover:underline">Оплата</a></li>
+            <li><a href="#" className="hover:underline">Доставка</a></li>
+            <li><a href="#" className="hover:underline">Реквизиты компании</a></li>
+            <li><a href="#" className="hover:underline">Правовая информация</a></li>
+            <li><a href="#" className="hover:underline">FAQ</a></li>
+          </ul>
+        </div>
+
+        {/* Четвертая колонка */}
+        <div className="flex flex-col items-start">
+          <h3 className="font-bold mb-2">Контакты</h3>
+          <ul className="list-none space-y-1">
+            <li className="flex items-center">
+              <a href="#" className="flex items-center">
+                <img src="/pin.svg" alt="Местоположение" className="mr-2" />
+                место магазина
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a href="tel:+74952335557" className="flex items-center hover:underline">
+                <img src="/phone.svg" alt="Телефон" className="mr-2" />
+                +7 (495) 233 55 57
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a href="mailto:info@i-lights.ru" className="flex items-center hover:underline">
+                <img src="/email.svg" alt="Email" className="mr-2" />
+                info@i-lights.ru
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import Menu from './Menu';
+import Container from './Container';
 
 const items = [
   "О компании",
@@ -8,30 +8,39 @@ const items = [
   "Проекты",
   "Доставка и оплата",
   "Контакты",
-  "Подборка"
 ];
-
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-[#0C0537] p-4 flex items-center justify-between">
-      <div className="flex items-center">
-      <img src="/logo-header.svg" alt="Логотип" className="h-10 mr-4" />
-      <Menu menuItems={items} />
-      </div>
-      <div className="flex items-center space-x-4">
-        <button className="bg-[#FFC94F] rounded-full p-2 flex items-center justify-center">
-       
-        </button>
-        <a href="mailto:info@i-lights.ru" className="text-white flex items-center">
-        
-          info@i-lights.ru
-        </a>
-        <button className="border border-white text-white flex items-center p-2">
-         
-          +7 (495) 233 55 57
-        </button>
-      </div>
+    <header className="bg-[#0C0537] p-4">
+      <Container>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <a href="/" className="flex items-center">
+              <img src="/logo-header.svg" alt="Логотип" className="h-10" />
+            </a>
+            <div className="ml-[84px]">
+              <Menu menuItems={items} />
+            </div>
+            <div className="flex items-center ml-4">
+              <button className="flex items-center space-x-2 text-white">
+                <span>Подборка</span>
+                <img src="/fav.svg" alt="Избранное" className="h-10" />
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center space-x-[40px]">
+            <button className="flex items-center space-x-2 text-white">
+              <img src="/email.svg" alt="Email" className="h-5" />
+              <span>info@i-lights.ru</span>
+            </button>
+            <button className="flex items-center space-x-2 border border-white text-white p-2">
+              <img src="/phone.svg" alt="Телефон" className="h-5" />
+              <span>+7 (495) 233 55 57</span>
+            </button>
+          </div>
+        </div>
+      </Container>
     </header>
   );
 };
