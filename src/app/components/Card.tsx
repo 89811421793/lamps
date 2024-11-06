7df4e1f0f34acc1f71b6958c2ef418e6759741a1
Maksim Dorogoi <monarch2013@mail.ru>
2024-11-06 16:53:07 +1100

    вид и фукц-ть карточек

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,47 +1,66 @@
-import React from 'react';
+import React from "react";
 
 type CardProps = {
-    url: string;
-    title: string;
-    price: number;
-    id: string;
-    features?: { id: string; text: string }[];
+  url: string;
+  title: string;
+  price: number;
+  id: string;
+  features?: { id: string; text: string }[];
 };
 
-const Card: React.FC<CardProps> = ({ url, title, price, id, features = [] }) => {
-    return (
-        <div className="border rounded-lg flex flex-col h-full mx-0"> {/* Убрано space-y-8 */}
-            <div className="flex justify-center items-center h-24 mb-4 pt-[180px]">
-                {url.includes('prod1') ? (
-                    <img src={url} alt={title} className="w-[256px] h-[96px] object-contain rounded-t-lg mb-[100px]" />
-                ) : (
-                    <img src={url} alt={title} className="w-[128px] h-[181px] object-contain rounded-t-lg mb-[100px]" />
-                )}
-            </div>
-            <h1 className="text-darkgrey font-bold text-lg leading-5 mb-[8px] p-0 mt-[80px] ml-[10px] mr-[10px]">{title}</h1> {/* Уменьшен паддинг */}
-            <ul className="list-disc pl-5 space-y-2 mb-4 p-0 mt-[8px] ml-[10px] mr-[10px]"> {/* Уменьшен паддинг */}
-                {features.map((feature) => (
-                    <li key={feature.id} className="text-shortDesc font-light text-sm leading-5">
-                        {feature.text}
-                    </li>
-                ))}
-            </ul>
-            <span className="text-darkgrey font-bold text-xl leading-5 mb-4 p-4">{price} ₽</span> 
-            <div className="flex w-full space-x-0">
-                <a 
-                    href={`/product/${id}`} 
-                    className="flex items-center justify-center bg-accent text-white font-semibold text-sm leading-5 h-10 flex-grow"
-                >
-                    Подробнее
-                </a>
-                <button 
-                    className="flex items-center justify-center border border-gray-300 bg-white text-darkgrey font-semibold text-sm leading-5 h-10 w-14"
-                >
-                    <img src="/add_grey.svg" alt="Добавить" className="mr-2" />
-                </button>
-            </div>
-        </div>
-    );
+const Card: React.FC<CardProps> = ({
+  url,
+  title,
+  price,
+  id,
+  features = [],
+}) => {
+  return (
+    <div className=" card border rounded-lg flex flex-col h-full mx-0 relative transition-shadow duration-300 group hover:shadow-[0px_0px_30px_0px_#FFC94F4D]">
+      <div className="flex justify-center items-center h-24 mb-4 pt-[180px]">
+        {url.includes("prod1") ? (
+          <img
+            src={url}
+            alt={title}
+            className="w-[256px] h-[96px] object-contain rounded-t-lg mb-[100px]"
+          />
+        ) : (
+          <img
+            src={url}
+            alt={title}
+            className="w-[128px] h-[181px] object-contain rounded-t-lg mb-[100px]"
+          />
+        )}
+      </div>
+      <h1 className="text-darkgrey font-bold text-lg leading-5 mb-[8px] p-0 mt-[80px] ml-[10px] mr-[10px]">
+        {title}
+      </h1>
+      <ul className="list-none pl-5 space-y-2 mb-4 p-0 mt-[8px] mr-[10px] custom-list">
+        {features.map((feature) => (
+          <li key={feature.id} className="flex items-start">
+            <span className="bg-secondary mr-2 w-[6px] h-[6px] rounded-full"></span>
+            <span className="text-shortDesc font-light text-sm leading-5">
+              {feature.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+      <span className="text-darkgrey font-bold text-xl leading-5 mb-4 p-4 transition-colors duration-300 group-hover:text-[#F3C650]">
+        {price} ₽
+      </span>
+      <div className="flex w-full space-x-0">
+        <a
+          href={`/product/${id}`}
+          className="flex items-center justify-center bg-accent text-white font-semibold text-sm leading-5 h-10 flex-grow"
+        >
+          Подробнее
+        </a>
+        <button className="flex items-center justify-center border border-gray-300 bg-white text-darkgrey font-semibold text-sm leading-5 h-10 w-14">
+          <img src="/add_grey.svg" alt="Добавить" className="mr-2" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/app/components/sections/mainpage/Latest.tsx
+++ src/app/components/sections/mainpage/Latest.tsx
@@ -1,88 +1,86 @@
-// app/latest/page.tsx (используйте папку app для серверных компонентов)
-
-import React from 'react';
-import SectionTitle from '../../SectionTitle';
-import Card from '../../Card';
+import React from "react";
+import SectionTitle from "../../SectionTitle";
+import Card from "../../Card";
 
 type CardProps = {
-    url: string;
-    title: string;
-    price: number;
-    id: string;
-    features?: { id: string; text: string }[];
+  url: string;
+  title: string;
+  price: number;
+  id: string;
+  features?: { id: string; text: string }[];
 };
 
 const getCards = async (): Promise<CardProps[]> => {
-    // Здесь вы можете получить данные с сервера
-    return [
-        {
-            url: '/prod1.png',
-            title: 'Дизайнерский светильник изготавливаемый по проекту',
-            price: 21300,
-            id: '1',
-            features: [
-                { id: '1', text: 'Длинна до 3 метров.' },
-                { id: '2', text: 'Углы поворота между секторами по проекту.' },
-                { id: '3', text: 'Мощность рассчитывается индивидуально.' },
-            ],
-        },
-        
-        {
-            url: '/prod2.png',
-            title: 'Дизайнерский светильник изготавливаемый по проекту',
-            price: 41500,
-            id: '2',
-            features: [
-                { id: '1', text: 'Длинна до 3 метров.' },
-                { id: '2', text: 'Углы поворота между секторами по проекту.' },
-                { id: '3', text: 'Мощность рассчитывается индивидуально.' },
-            ],
-        },
-        {
-            url: '/prod1.png',
-            title: 'Дизайнерский светильник изготавливаемый по проекту',
-            price: 21800,
-            id: '3',
-            features: [
-                { id: '1', text: 'Длинна до 3 метров.' },
-                { id: '2', text: 'Углы поворота между секторами по проекту.' },
-                { id: '3', text: 'Мощность рассчитывается индивидуально.' },
-            ],
-        },
-        {
-            url: '/prod1.png',
-            title: 'Дизайнерский светильник изготавливаемый по проекту',
-            price: 37900,
-            id: '4',
-            features: [
-                { id: '1', text: 'Длинна до 3 метров.' },
-                { id: '2', text: 'Углы поворота между секторами по проекту.' },
-                { id: '3', text: 'Мощность рассчитывается индивидуально.' },
-            ],
-        },
-    ];
+  // получаем здесь данные с сервера
+  return [
+    {
+      url: "/prod1.png",
+      title: "Дизайнерский светильник изготавливаемый по проекту",
+      price: 21300,
+      id: "1",
+      features: [
+        { id: "1", text: "Длинна до 3 метров." },
+        { id: "2", text: "Углы поворота между секторами по проекту." },
+        { id: "3", text: "Мощность рассчитывается индивидуально." },
+      ],
+    },
+
+    {
+      url: "/prod2.png",
+      title: "Дизайнерский светильник изготавливаемый по проекту",
+      price: 41500,
+      id: "2",
+      features: [
+        { id: "1", text: "Длинна до 3 метров." },
+        { id: "2", text: "Углы поворота между секторами по проекту." },
+        { id: "3", text: "Мощность рассчитывается индивидуально." },
+      ],
+    },
+    {
+      url: "/prod1.png",
+      title: "Дизайнерский светильник изготавливаемый по проекту",
+      price: 21800,
+      id: "3",
+      features: [
+        { id: "1", text: "Длинна до 3 метров." },
+        { id: "2", text: "Углы поворота между секторами по проекту." },
+        { id: "3", text: "Мощность рассчитывается индивидуально." },
+      ],
+    },
+    {
+      url: "/prod1.png",
+      title: "Дизайнерский светильник изготавливаемый по проекту",
+      price: 37900,
+      id: "4",
+      features: [
+        { id: "1", text: "Длинна до 3 метров." },
+        { id: "2", text: "Углы поворота между секторами по проекту." },
+        { id: "3", text: "Мощность рассчитывается индивидуально." },
+      ],
+    },
+  ];
 };
 
 const Latest = async () => {
-    const cards = await getCards(); // Получаем карточки асинхронно
+  const cards = await getCards(); // Получаем карточки асинхронно
 
-    return (
-        <section className="mb-[20px]">
-            <SectionTitle title="Новинки" />
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-                {cards.map(card => (
-                    <Card
-                        key={card.id}
-                        url={card.url}
-                        title={card.title}
-                        price={card.price}
-                        id={card.id}
-                        features={card.features}
-                    />
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section className="mb-[20px]">
+      <SectionTitle title="Новинки" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+        {cards.map((card) => (
+          <Card
+            key={card.id}
+            url={card.url}
+            title={card.title}
+            price={card.price}
+            id={card.id}
+            features={card.features}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Latest;
