a068706f72bed67affd9f59efa3f8a4827ab9dde
Maksim Dorogoi <monarch2013@mail.ru>
2024-11-06 15:27:18 +1100

    tailwind классы к карточкам

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -11,11 +11,11 @@ type CardProps = {
 const Card: React.FC<CardProps> = ({ url, title, price, id, features = [] }) => {
     return (
         <div className="border rounded-lg flex flex-col h-full space-y-8 mx-0"> 
-            <div className="flex justify-center items-center h-24 mb-4 pt-[50px]">
+            <div className="flex justify-center items-center h-24 mb-4 pt-[180px]">
                 {url.includes('prod1') ? (
-                    <img src={url} alt={title} className="w-[256px] h-full object-contain rounded-t-lg" />
+                    <img src={url} alt={title} className="w-[256px] h-[96px] object-contain rounded-t-lg mb-[100px]" />
                 ) : (
-                    <img src={url} alt={title} className="w-[128px] h-full object-contain rounded-t-lg" />
+                    <img src={url} alt={title} className="w-[128px] h-[181px] object-contain rounded-t-lg mb-[100px]" />
                 )}
             </div>
             <h1 className="text-darkgrey font-bold text-lg leading-5 mb-4 p-4">{title}</h1> 
