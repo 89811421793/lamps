21b5744ee471d0455722ee44743e1ed44c93c1f8
Maksim Dorogoi <monarch2013@mail.ru>
2024-11-09 19:15:48 +1100

    breadcrumbs (страница о компании)

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
+import Container from '@/app/components/Container';
 
 const About = () => {
     return (
         <div>
-            Вторая страница (О Компании)
+            <Container>
+            <nav className="text-[#7C7C7C]">
+                <Link href="/" className="hover:text-gray-800">
+                    Главная
+                </Link>
+                <span className="mx-2">-</span>
+                <Link href="/about" className="hover:text-gray-800">
+                    О компании
+                </Link>
+            </nav>
+            </Container>
         </div>
     );
 };
-
 export default About;
