a33c4ab91cdddc5f034616f252fe1f83f730b3f6
Maksim Dorogoi <monarch2013@mail.ru>
2024-11-13 15:51:04 +1100

    prev tabs list for catalog page (temp. "use client")

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,9 +1,27 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Container from "../components/Container";
 import SectionTitle from "../components/SectionTitle";
 
+const tabs = [
+  "Трековые светильники",
+  "Встраиваемые светильники",
+  "Профильные светильники",
+  "Фигурные светильники",
+  "Светильники Армстронг",
+  "Светильники Армстронг с равномерной засветкой",
+];
+
 const Catalog = () => {
+  const [selectedTab, setSelectedTab] = useState<string>(tabs[0]); // Изначально выбран первый таб
+
+  const handleTabClick = (tab: string) => {
+    if (selectedTab !== tab) {
+      setSelectedTab(tab);
+    }
+  };
+
   return (
     <div>
       <Container>
@@ -21,6 +39,30 @@ const Catalog = () => {
           showIndicator={false}
           className="mb-[40px]"
         />
+        
+        {/* Контейнер для табов */}
+        <ul className="mb-4 mt-6 list-none p-0">
+          {tabs.map((tab, index) => (
+            <li
+              key={tab}
+              onClick={() => handleTabClick(tab)}
+              className={`border border-[#E5E5E5] cursor-pointer py-2 px-4 rounded-[3px] inline-block leading-[18px] ${
+                selectedTab === tab
+                  ? "text-[var(--whiteFont)] bg-[var(--accent)] font-semibold" 
+                  : "text-[var(--darkgrey)] font-medium" 
+              }`}
+              style={{
+                fontFamily: 'Montserrat',
+                fontSize: '14px',
+                marginRight: index < 5 ? '10px' : '0', 
+                marginBottom: '10px', 
+                fontWeight: selectedTab === tab ? 600 : 500, 
+              }}
+            >
+              {tab}
+            </li>
+          ))}
+        </ul>
       </Container>
     </div>
   );
