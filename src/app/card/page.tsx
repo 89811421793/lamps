f325095b6d512acd3c6dfebfcd5f3430371e704b
Maksim Dorogoi <monarch2013@mail.ru>
2024-11-20 18:02:30 +1100

    список табов (ul вместо div)

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -30,13 +30,21 @@ const Card: React.FC = () => {
     <div className="flex">
       <Container>
         <nav className="text-[#7C7C7C] mt-[17px] mb-[38px]">
-          <Link href="/" className="hover:text-gray-800">Главная</Link>
+          <Link href="/" className="hover:text-gray-800">
+            Главная
+          </Link>
           <span className="mx-2">-</span>
-          <Link href="/catalog" className="hover:text-gray-800">Каталог</Link>
+          <Link href="/catalog" className="hover:text-gray-800">
+            Каталог
+          </Link>
           <span className="mx-2">-</span>
-          <Link href="#" className="hover:text-gray-800">Трековые светильники</Link>
+          <Link href="#" className="hover:text-gray-800">
+            Трековые светильники
+          </Link>
           <span className="mx-2">-</span>
-          <Link href="#" className="hover:text-gray-800">EVO L</Link>
+          <Link href="#" className="hover:text-gray-800">
+            EVO L
+          </Link>
         </nav>
         <div className="flex mb-[50px]">
           <VerticalSlider onSlideChange={handleSlideChange} />
@@ -44,49 +52,134 @@ const Card: React.FC = () => {
             <h2 className="text-xl font-bold">Трековый светильник EVO L</h2>
             <div className="border-b border-[#EDEDED] my-[28px]" />
             <div className="mb-[16px]">
-
               {/* Блок характеристик */}
               <div className="flex mb-2">
                 <span className="w-[30%]">Мощность</span>
                 <div className="flex gap-[10px] flex-wrap max-w-[45%]">
-                  <span className={`border ${isLampScrSelected ? 'border-secondary' : 'border-[#EDEDED]'} text ${isLampScrSelected ? 'text-darkgrey' : 'text-[var(--shortDesc)]'} px-[10px] py-[5px]`}>15</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">22</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">28</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">42</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">43</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">65</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">66</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">86</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">84</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">130</span>
+                  <span
+                    className={`border ${
+                      isLampScrSelected
+                        ? "border-secondary"
+                        : "border-[#EDEDED]"
+                    } text ${
+                      isLampScrSelected
+                        ? "text-darkgrey"
+                        : "text-[var(--shortDesc)]"
+                    } px-[10px] py-[5px]`}
+                  >
+                    15
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    22
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    28
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    42
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    43
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    65
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    66
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    86
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    84
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    130
+                  </span>
                 </div>
               </div>
               <div className="flex mb-2">
                 <span className="w-[30%]">Цвет корпуса</span>
                 <div className="flex w-[70%] gap-[10px]">
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">белый</span>
-                  <span className={`border ${isLampScrSelected ? 'border-secondary' : 'border-[#EDEDED]'} text ${isLampScrSelected ? 'text-darkgrey' : 'text-[var(--shortDesc)]'} px-[10px] py-[5px]`}>черный</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">серый</span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    белый
+                  </span>
+                  <span
+                    className={`border ${
+                      isLampScrSelected
+                        ? "border-secondary"
+                        : "border-[#EDEDED]"
+                    } text ${
+                      isLampScrSelected
+                        ? "text-darkgrey"
+                        : "text-[var(--shortDesc)]"
+                    } px-[10px] py-[5px]`}
+                  >
+                    черный
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    серый
+                  </span>
                 </div>
               </div>
               <div className="flex mb-2">
                 <span className="w-[30%]">Угол рассеивания</span>
                 <div className="w-[70%]">
-                  <span className={`border ${isLampScrSelected ? 'border-secondary' : 'border-[#EDEDED]'} text ${isLampScrSelected ? 'text-darkgrey' : 'text-[var(--shortDesc)]'} px-[10px] py-[5px]`}>85</span>
+                  <span
+                    className={`border ${
+                      isLampScrSelected
+                        ? "border-secondary"
+                        : "border-[#EDEDED]"
+                    } text ${
+                      isLampScrSelected
+                        ? "text-darkgrey"
+                        : "text-[var(--shortDesc)]"
+                    } px-[10px] py-[5px]`}
+                  >
+                    85
+                  </span>
                 </div>
               </div>
               <div className="flex mb-2">
                 <span className="w-[30%]">Цветовая температура</span>
                 <div className="flex w-[70%] gap-[10px] items-center">
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">3000</span>
-                  <span className={`border ${isLampScrSelected ? 'border-secondary' : 'border-[#EDEDED]'} text ${isLampScrSelected ? 'text-darkgrey' : 'text-[var(--shortDesc)]'} px-[10px] py-[5px]`}>4000</span>
-                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">5000</span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    3000
+                  </span>
+                  <span
+                    className={`border ${
+                      isLampScrSelected
+                        ? "border-secondary"
+                        : "border-[#EDEDED]"
+                    } text ${
+                      isLampScrSelected
+                        ? "text-darkgrey"
+                        : "text-[var(--shortDesc)]"
+                    } px-[10px] py-[5px]`}
+                  >
+                    4000
+                  </span>
+                  <span className="border border-[#EDEDED] text-[var(--shortDesc)] px-[10px] py-[5px]">
+                    5000
+                  </span>
                 </div>
               </div>
               <div className="flex mb-2">
                 <span className="w-[30%]">CRI/Ra</span>
                 <div className="w-[70%]">
-                  <span className={`border ${isLampScrSelected ? 'border-secondary' : 'border-[#EDEDED]'} text ${isLampScrSelected ? 'text-darkgrey' : 'text-[var(--shortDesc)]'} px-[10px] py-[5px]`}>80+</span>
+                  <span
+                    className={`border ${
+                      isLampScrSelected
+                        ? "border-secondary"
+                        : "border-[#EDEDED]"
+                    } text ${
+                      isLampScrSelected
+                        ? "text-darkgrey"
+                        : "text-[var(--shortDesc)]"
+                    } px-[10px] py-[5px]`}
+                  >
+                    80+
+                  </span>
                 </div>
               </div>
             </div>
@@ -100,35 +193,55 @@ const Card: React.FC = () => {
           </div>
         </div>
 
-         {/* Tabs Section */}
-         <div className="flex mb-[30px]">
+        {/* Tabs Section */}
+        <ul className="flex mb-[30px] list-none p-0">
           {tabs.map((tab) => (
-            <button
+            <li
               key={tab.id}
               onClick={() => handleTabChange(tab.id)}
-              className={`text-[var(--darkgrey)] font-medium mr-[60px] ${
-                selectedTab === tab.id 
-                  ? 'bg-[var(--accent)] text-[var(--whiteFont)] py-2 px-4' 
-                  : ''
+              className={`mr-[60px] cursor-pointer text-[var(--darkgrey)] font-medium ${
+                selectedTab === tab.id
+                  ? "bg-[var(--accent)] text-[var(--whiteFont)] py-2 px-4"
+                  : ""
               }`}
             >
               {tab.label}
-            </button>
+            </li>
           ))}
-        </div>
+        </ul>
 
         {/* Additional Content Area */}
         <div className="flex flex-col mb-[50px]">
           <p className="text-[var(--shortDesc)] mb-[50px]">
-            Потолочный светильник LINE A подвесного или накладного монтажа предназначен для организации основного освещения помещений. Различные длины, мощности, варианты рассеивателей и возможности подключения к различным системам управления освещением делают светильники серии LINE A всесторонне продуманным решением для помещений любой конфигурации и функциональных назначений. Алюминиевый корпус окрашивается высококачественным матовым или глянцевым порошковым покрытием черного, белого, либо любого цвета по шкале RAL.
+            Потолочный светильник LINE A подвесного или накладного монтажа
+            предназначен для организации основного освещения помещений.
+            Различные длины, мощности, варианты рассеивателей и возможности
+            подключения к различным системам управления освещением делают
+            светильники серии LINE A всесторонне продуманным решением для
+            помещений любой конфигурации и функциональных назначений.
+            Алюминиевый корпус окрашивается высококачественным матовым или
+            глянцевым порошковым покрытием черного, белого, либо любого цвета по
+            шкале RAL.
           </p>
-          
+
           <h3 className="font-bold mb-2">Особенности</h3>
           <ul className="list-disc ml-5 mb-[50px]">
-            <li className="text-[var(--shortDesc)]">Различные варианты рассеивателей: матовый / микропризматический / прозрачный.</li>
-            <li className="text-[var(--shortDesc)]">Источники питания с гальваниеской развязкой установлены внутри корпуса светильника;</li>
-            <li className="text-[var(--shortDesc)]">Возможность соединения последовательно для получения сплошных линий засветки длиной до 30м.</li>
-            <li className="text-[var(--shortDesc)]">Возможность декорирования под дерево и другие материалы, либо отделка деревом благородных пород.</li>
+            <li className="text-[var(--shortDesc)]">
+              Различные варианты рассеивателей: матовый / микропризматический /
+              прозрачный.
+            </li>
+            <li className="text-[var(--shortDesc)]">
+              Источники питания с гальваниеской развязкой установлены внутри
+              корпуса светильника;
+            </li>
+            <li className="text-[var(--shortDesc)]">
+              Возможность соединения последовательно для получения сплошных
+              линий засветки длиной до 30м.
+            </li>
+            <li className="text-[var(--shortDesc)]">
+              Возможность декорирования под дерево и другие материалы, либо
+              отделка деревом благородных пород.
+            </li>
           </ul>
 
           <h3 className="font-bold mb-2">Области применения:</h3>
@@ -136,8 +249,12 @@ const Card: React.FC = () => {
             <li className="text-[var(--shortDesc)]">Торговое освещение</li>
             <li className="text-[var(--shortDesc)]">Офисное освещение</li>
             <li className="text-[var(--shortDesc)]">Объекты бьюти индустрии</li>
-            <li className="text-[var(--shortDesc)]">HoReCa - отели, рестораны, кафе</li>
-            <li className="text-[var(--shortDesc)]">Проходные зоны любых общественных пространств</li>
+            <li className="text-[var(--shortDesc)]">
+              HoReCa - отели, рестораны, кафе
+            </li>
+            <li className="text-[var(--shortDesc)]">
+              Проходные зоны любых общественных пространств
+            </li>
           </ul>
         </div>
       </Container>
