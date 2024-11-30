3faf93129f35409ea64a92bbfdf2a207e29981c2
Maksim Dorogoi <monarch2013@mail.ru>
2024-11-30 16:04:19 +1100

    push

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,12 +1,12 @@
 // src/store/store.ts
 import { createStore, combineReducers } from 'redux';
-import cartReducer from './reducer'; // Импортируем редюсер
+import cartReducer from './reducer'; 
 
 const rootReducer = combineReducers({
   cart: cartReducer,
 });
 
-export type RootState = ReturnType<typeof rootReducer>; // Экспортируем RootState
+export type RootState = ReturnType<typeof rootReducer>; 
 
 const store = createStore(rootReducer);
 
